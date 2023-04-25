d519eaf8a70fc63ade386f10d40c5f74673710b3
49M <74563947+49M@users.noreply.github.com>
2023-04-25 16:43:50 -0400

    typing added
    - added typing feature where it automatically moves to the next text field after clicking a key.
    - Still has a lot of work to improve such as bug where it starts at the second key and last key letters are not capitalized.
    - might change the whole code still so this is just a rough draft

--- src/main/java/MainGameController.java
+++ src/main/java/MainGameController.java
@@ -2,7 +2,36 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 
 public class MainGameController {
     
+    @FXML
+    TextField zeroZero, zeroOne, zeroTwo, zeroThree, zeroFour, oneZero, oneOne, oneTwo, oneThree, oneFour, twoZero, twoOne, twoTwo,
+    twoThree, twoFour, threeZero, threeOne, threeTwo, threeThree, threeFour, fourZero, fourOne, fourTwo, fourThree, fourFour,
+    fiveZero, fiveOne, fiveTwo, fiveThree, fiveFour;
+    
+    @FXML
+    public void keyPressed(){
+
+        TextField[] letterSlots = {zeroZero, zeroOne, zeroTwo, zeroThree, zeroFour, oneZero, oneOne, oneTwo, oneThree, oneFour, twoZero, twoOne, twoTwo,
+            twoThree, twoFour, threeZero, threeOne, threeTwo, threeThree, threeFour, fourZero, fourOne, fourTwo, fourThree, fourFour,
+            fiveZero, fiveOne, fiveTwo, fiveThree, fiveFour};
+
+        for (int i = 0; i <= letterSlots.length - 1; i++) {
+            TextField letter1 = letterSlots[i];
+            TextField letter2 = letterSlots[i + 1];
+            letterSlots[i].setTextFormatter(new TextFormatter<>((change) -> {
+                change.setText(change.getText().toUpperCase());
+                return change;
+            }));
+
+            letterSlots[i].setOnKeyPressed(event -> {
+                if (letter1.getLength() >= 1){
+                    letter2.requestFocus();
+                }
+            });            
+        }
+    }
 }
