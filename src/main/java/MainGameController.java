a4e11b2ba0cfc3170d2232955e5918afbf686ff3
49M <74563947+49M@users.noreply.github.com>
2023-04-29 17:28:22 -0400

    non character detection improvement
    - made non-alphabetic characters disappear
    - fixed bug for the first slot, makes sure that slot cannot be subtracted if slot = 0 (prevents from slot being equal to -1)

--- src/main/java/MainGameController.java
+++ src/main/java/MainGameController.java
@@ -11,8 +11,10 @@ import javafx.scene.input.KeyEvent;
 
 public class MainGameController {
 
-    public int slot = -1;
+    public int slot = 0;
     public boolean nonChar = false; 
+    public boolean firstSlot = true;
+
     @FXML
     TextField zeroZero, zeroOne, zeroTwo, zeroThree, zeroFour, oneZero, oneOne, oneTwo, oneThree, oneFour, twoZero, twoOne, twoTwo,
     twoThree, twoFour, threeZero, threeOne, threeTwo, threeThree, threeFour, fourZero, fourOne, fourTwo, fourThree, fourFour,
@@ -28,14 +30,17 @@ public class MainGameController {
         // Get the key that was pressed
         // If the key is a letter, it sets the text of the current TextField to the letter and move to the next TextField
         if(event.getCode().isLetterKey()) {
-            System.out.println("Character");
-            slot++;
-            System.out.println(slot);
+            if (!firstSlot){
+                slot++;
+            } else {firstSlot = false;}
+
             if (!nonChar){
-                letterSlots[slot].clear();
+                //letterSlots[slot].clear();
                 letterSlots[slot].requestFocus();
             } else {
-                slot--;
+                if (slot != 0){
+                    slot--;
+                }
                 letterSlots[slot].clear();
                 letterSlots[slot].requestFocus();
                 nonChar = false;
@@ -49,12 +54,17 @@ public class MainGameController {
         if (slot == letterSlots.length - 1) {
             slot = -1;
         }
+        System.out.println(slot);
     }
 
     @FXML
-    public int keyReleased(KeyEvent event){
-        return slot;
+    public void keyReleased(KeyEvent event){
+        TextField[] letterSlots = {zeroZero, zeroOne, zeroTwo, zeroThree, zeroFour, oneZero, oneOne, oneTwo, oneThree, oneFour, twoZero, twoOne, twoTwo,
+            twoThree, twoFour, threeZero, threeOne, threeTwo, threeThree, threeFour, fourZero, fourOne, fourTwo, fourThree, fourFour,
+            fiveZero, fiveOne, fiveTwo, fiveThree, fiveFour};
+        if (nonChar){
+            letterSlots[slot].clear();
+        }
     }
-
 }
 
