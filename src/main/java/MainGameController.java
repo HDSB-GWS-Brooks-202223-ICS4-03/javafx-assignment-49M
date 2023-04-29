3c0f89c51afb2d9552c108ef1a284f28a92e6eaf
49M <74563947+49M@users.noreply.github.com>
2023-04-28 21:28:23 -0400

    Typing character check
    - Made the program distinguish between alphabet keys vs non-alphabet keys (numbers, etc) and only moves to the next text box if it is an alphabetical character. If not, it waits until an acceptable key is typed before moving forwards.

--- src/main/java/MainGameController.java
+++ src/main/java/MainGameController.java
@@ -1,61 +1,60 @@
 import java.io.IOException;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
+import javafx.scene.input.KeyEvent;
 
 public class MainGameController {
 
     public int slot = -1;
-    
+    public boolean nonChar = false; 
     @FXML
     TextField zeroZero, zeroOne, zeroTwo, zeroThree, zeroFour, oneZero, oneOne, oneTwo, oneThree, oneFour, twoZero, twoOne, twoTwo,
     twoThree, twoFour, threeZero, threeOne, threeTwo, threeThree, threeFour, fourZero, fourOne, fourTwo, fourThree, fourFour,
     fiveZero, fiveOne, fiveTwo, fiveThree, fiveFour;
     
     @FXML
-    public void keyPressed(){
+    public void keyPressed(KeyEvent event){
 
         TextField[] letterSlots = {zeroZero, zeroOne, zeroTwo, zeroThree, zeroFour, oneZero, oneOne, oneTwo, oneThree, oneFour, twoZero, twoOne, twoTwo,
             twoThree, twoFour, threeZero, threeOne, threeTwo, threeThree, threeFour, fourZero, fourOne, fourTwo, fourThree, fourFour,
             fiveZero, fiveOne, fiveTwo, fiveThree, fiveFour};
 
-        // for (int i = 0; i <= letterSlots.length - 1; i++) {
-        //     TextField letter1 = letterSlots[i];
-        //     TextField letter2 = letterSlots[i + 1];
-        //     letterSlots[i].setTextFormatter(new TextFormatter<>((change) -> {
-        //         change.setText(change.getText().toUpperCase());
-        //         return change;
-        //     }));
-
-        //     letterSlots[i].setOnKeyPressed(event -> {
-        //         if (letter1.getLength() >= 1){
-        //             letter2.requestFocus();
-        //         }
-        //     });            
-        // }
-
         // Get the key that was pressed
-        String keyPressed = "a";
-        slot++;
-        // If the key is a letter, set the text of the current TextField to the letter and move to the next TextField
-        if (keyPressed.matches("[a-zA-Z]") && slot > 0) {
-            // letterSlots[slot].setText(keyPressed);
-            // System.out.println(slot);
-            letterSlots[slot].requestFocus();
+        // If the key is a letter, it sets the text of the current TextField to the letter and move to the next TextField
+        if(event.getCode().isLetterKey()) {
+            System.out.println("Character");
+            slot++;
+            System.out.println(slot);
+            if (!nonChar){
+                letterSlots[slot].clear();
+                letterSlots[slot].requestFocus();
+            } else {
+                slot--;
+                letterSlots[slot].clear();
+                letterSlots[slot].requestFocus();
+                nonChar = false;
+            }
+        } else {
+            nonChar = true;
+            letterSlots[slot].clear();
         }
 
         // If all the text fields have been filled, reset the slot counter to 0
-        if (slot >= letterSlots.length - 1) {
-            slot = 0;
+        if (slot == letterSlots.length - 1) {
+            slot = -1;
         }
-        //zeroOne.requestFocus();
     }
 
     @FXML
-    public int keyReleased(){
+    public int keyReleased(KeyEvent event){
         return slot;
     }
+
 }
 
