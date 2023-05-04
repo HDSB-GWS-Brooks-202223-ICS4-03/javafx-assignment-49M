c973acd2c2ddb39e23239dcb6513d4c62b77efad
49M <74563947+49M@users.noreply.github.com>
2023-05-04 17:47:48 -0400

    Update MainGameController.java

--- src/main/java/MainGameController.java
+++ src/main/java/MainGameController.java
@@ -21,24 +21,27 @@ public class MainGameController {
     private String wordleWord;
 
     // reference used from: https://stackoverflow.com/questions/12028205/randomly-choose-a-word-from-a-text-file#:~:text=To%20get%20the%20words%20use,yourRandom%20%3D%20new%20Random(words.
+    // This code reads the list of words in the "wordleWordListClean" txt file and then randomly choses a word from the list to save to the randomWord variable
     public String readFromFile() throws IOException {
-            BufferedReader reader = new BufferedReader(new FileReader("wordleWordListClean.txt"));
-            String line = reader.readLine();
-            List<String> words = new ArrayList<String>();
-            while(line != null) {
-                String[] wordsLine = line.split(" ");
-                for(String word : wordsLine) {
-                    words.add(word);
-                }
-                line = reader.readLine();
-        }
-         Random rand = new Random(System.currentTimeMillis());
-         String randomWord = words.get(rand.nextInt(words.size()));
-         System.out.println(randomWord);
-         return randomWord;
+            try (BufferedReader reader = new BufferedReader(new FileReader("wordleWordListClean.txt"))) {
+                String line = reader.readLine();
+                List<String> words = new ArrayList<String>();
+                while(line != null) {
+                    String[] wordsLine = line.split(" ");
+                    for(String word : wordsLine) {
+                        words.add(word);
+                    }
+                    line = reader.readLine();
+      }
+       Random rand = new Random(System.currentTimeMillis());
+       String randomWord = words.get(rand.nextInt(words.size()));
+       System.out.println(randomWord);
+       return randomWord;
+            }
     }
 
     @FXML
+    //  intitialize method calls the readFromFile method to get the wordle word for the round (initialize method runs at the start)
     public  void initialize(){
         try {
             wordleWord = readFromFile();
@@ -48,11 +51,13 @@ public class MainGameController {
     }
 
     @FXML
+    //  text field fx:id's
     TextField zeroZero, zeroOne, zeroTwo, zeroThree, zeroFour, oneZero, oneOne, oneTwo, oneThree, oneFour, twoZero, twoOne, twoTwo,
     twoThree, twoFour, threeZero, threeOne, threeTwo, threeThree, threeFour, fourZero, fourOne, fourTwo, fourThree, fourFour,
     fiveZero, fiveOne, fiveTwo, fiveThree, fiveFour;
     
     @FXML
+    //  runs when the event is keyPressed
     public void keyPressed(KeyEvent event){
 
         TextField[] letterSlots = {zeroZero, zeroOne, zeroTwo, zeroThree, zeroFour, oneZero, oneOne, oneTwo, oneThree, oneFour, twoZero, twoOne, twoTwo,
@@ -60,18 +65,20 @@ public class MainGameController {
             fiveZero, fiveOne, fiveTwo, fiveThree, fiveFour};
 
         // Get the key that was pressed
-        // If the key is a letter, it sets the text of the current TextField to the letter and move to the next TextField
+        // If the key pressed is a letter, it is typed in the current text field and then focuses on the next field
         if(event.getCode().isLetterKey()) {
             firstBackSpace = true;
+            //  runs if there is no non letter (ex; number) present in the text field
             if (!numberInField) {
+                //  if slot is not the first text field, the if statement is executed
                 if (!firstSlot){
                     slot++;
                     if (slot == 30) {
                         slot = 0;
                     }
-                    System.out.println("this runs");
-                } else {firstSlot = false;}
+                } else {firstSlot = false;} //  setting firsSlot to false allows the preceding if statement to run and move past slot 1
     
+                // if it is an alphabetic character this runs
                 if (!nonChar){
                     letterSlots[slot].requestFocus();
                 } else {
@@ -81,14 +88,17 @@ public class MainGameController {
                     nonChar = false;
                 }
                 System.out.println(wordleWord);
+
                 // reference used from: https://stackoverflow.com/questions/30884812/javafx-textfield-automatically-transform-text-to-uppercase
+                // changes all typed letters to uppercase
                 letterSlots[slot].setTextFormatter(new TextFormatter<>((change) -> {
                     change.setText(change.getText().toUpperCase());
                     return change;
                 }));
             }
 
-        } else if(event.getCode() == KeyCode.BACK_SPACE){
+        } else if(event.getCode() == KeyCode.BACK_SPACE){ //  Backspaces the letters and moves the focus on textfields backwards
+            // Checking if firstBackSpace is false makes sure that the backspace deletes the current text before the preceding one 
             if (!firstBackSpace && slot > 0) {
                 slot --;
             } else if (slot > 1){
@@ -97,28 +107,25 @@ public class MainGameController {
             if (slot == 0) {
                 firstSlot = true;
             }
+            // clears the text field and focuses on the slot behind the current one (if !firstBackSpace)
             letterSlots[slot].clear();
             letterSlots[slot].requestFocus();
             firstSlot = true;
-        } else {
+        } else { // this is executed when the pressed key is neither a letter or backspace
             nonChar = true;
             firstBackSpace = false;
             numberInField = true;
         }
-
-        // If all the text fields have been filled, reset the slot counter to 0
-        // if (slot == letterSlots.length - 1) {
-        //     slot = -1;
-        // }
-        System.out.println(slot);
     }
 
     @FXML
-    public void keyReleased(KeyEvent event){
+    // when the key pressed is released
+    public void keyReleased(KeyEvent event){ 
         TextField[] letterSlots = {zeroZero, zeroOne, zeroTwo, zeroThree, zeroFour, oneZero, oneOne, oneTwo, oneThree, oneFour, twoZero, twoOne, twoTwo,
             twoThree, twoFour, threeZero, threeOne, threeTwo, threeThree, threeFour, fourZero, fourOne, fourTwo, fourThree, fourFour,
             fiveZero, fiveOne, fiveTwo, fiveThree, fiveFour};
-        if (nonChar){
+            // when a non char is typed this clears it because it is invalid
+            if (nonChar){
             letterSlots[slot].clear();
             letterSlots[slot].requestFocus();
             numberInField = false;
